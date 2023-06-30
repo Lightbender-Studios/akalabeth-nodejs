f16528dd1ea961b8a9267f53a13bf998052ef31b
Lightbender255 <lightbender255@gmail.com>
2023-06-30 07:01:19 -0600

    Working on Flowcharting the BASIC sourcecode

--- js/akalabeth-phaser3-typescript/src/akalabeth_scene.js
+++ js/akalabeth-phaser3-typescript/src/akalabeth_scene.js
@@ -12,12 +12,20 @@ export default class HelloWorldScene extends Phaser.Scene {
 		// loading assets from a CDN, this is how you tell Phaser where to look.
 		//
 
-		this.load.setBaseURL('https://labs.phaser.io')
-		this.load.image('sky', 'assets/skies/space3.png')
+		// this.load.setBaseURL('https://labs.phaser.io')
+		// this.load.image('sky', 'assets/skies/space3.png')
 
 	}
 
 	create() {
-		this.add.image(400, 300, 'sky')
+		let X1 = 0;
+		let Y1 = 0;
+		let X2 = 800;
+		let Y2 = 600;
+		//let ZZ = Math.random(-Math.abs())
+		// Draw a line between the top left and bottom right of the screen
+		this.add.line(X1 + 30, Y1 + 30, X1 + 20, Y1 + 30, 0, 600, 800, 0xffffff)
+		// Draw a line between the bottom left and top right of the screen
+		this.add.line(0, 600, 0, 600, 800, 0, 0xffffff)
 	}
 }
